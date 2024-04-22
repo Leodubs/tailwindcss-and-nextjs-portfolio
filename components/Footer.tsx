e748d59331df6b45aadf35a561c8871fb7168e16
Leodubs <108219809+Leodubs@users.noreply.github.com>
2024-04-22 18:14:42 +0200

    Update Footer.tsx
    display="none" all icons & links except linkedin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,17 +12,22 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Hosna Qasmei<a href="/" className="hover:underline"></a>
+{/* we might want to getyear automatically */}
+          © 2024 Eleonore Dubois<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a display="none" 
+            href="https://github.com/Leodubs" 
+            rel="noreferrer" 
+            target="_blank"
+          >
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
           </a>
-          <a
-            href="https://twitter.com/hqasmei"
+          <a display="none"
+            href="https://twitter.com/"
             rel="noreferrer"
             target="_blank"
           >
@@ -33,7 +38,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://www.linkedin.com/in/hosnaqasmei/"
+            href="https://www.linkedin.com/in/eleonore-dbs/"
             rel="noreferrer"
             target="_blank"
           >
@@ -42,7 +47,7 @@ const Footer = () => {
               size={30}
             />
           </a>
-          <a
+          <a display="none"
             href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
             rel="noreferrer"
             target="_blank"
